d5fcaaf226e5f379ffa315fac0859dd70bbd1030
Dan Bednarczyk <daniel.bednarczyk@paramount.com>
2026-03-25 15:01:27 -0400

    2. No errors in the console.

--- components/AthleteTable.tsx
+++ components/AthleteTable.tsx
@@ -20,7 +20,7 @@ export const AthleteTable = ({ athletes, currentAthlete, setCurrentAthlete }: At
     <tbody>
       {athletes.length > 0 ? (
         athletes.map(athlete => (
-          <tr key={athlete.position}>
+          <tr key={athlete.id}>
             <td>{athlete.name}</td>
             <td>{athlete.position}</td>
             <td>
